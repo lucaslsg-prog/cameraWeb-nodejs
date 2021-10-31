97cb132f6b78430ad66f64781aff3c7a7a8621a6
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2021-10-30 21:58:43 -0300

    adjust to deploy node application using heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = __dirname + '/views/'
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     
